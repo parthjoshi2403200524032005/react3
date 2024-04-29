9270d509cbbfe6f29e07f0ba004b10c0da6e02b6
Parth <parthjohi009@gmail.com>
2024-04-29 18:42:16 +0530

    first commit

---
+++ parth2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import { New } from './component1/one';
+
+function App() {
+  return (
+    <>
+   <New/>
+    </>
+  );
+}
+
+export default App;
---
+++ parth2/src/component1/one.jsx
@@ -0,0 +1,246 @@
+import React from "react";
+import image1 from "./image/Jane Cooper.avif";
+import image2 from "./image/Cody Fisher.avif";
+import image3 from "./image/Esther Howard.avif";
+import image4 from "./image/Jenny Wilson4.avif";
+import image5 from "./image/Kristin Watson5.avif";
+import image6 from "./image/Cameron Williamso6.avif";
+
+export const New = () => {
+  return (
+    <>
+      <div className="bg-[#F3F4F6] border-[1px] rounded-lg grid grid-rows-2 grid-cols-3 justify-center w-[1500px] h-[400px] pr-[100px] ml-[90px] mt-[200px]">
+        <div className="bg-[#FFFFFF] h-[140px] w-[400px] rounded-lg mt-[30px] ml-[90px]  ">
+          <div className=" p-[20px]">
+            Jane Cooper
+            <button className="bg-[#F0FDF4] text-[#15803D] border-[#C4ECD2] border-[1px] rounded-[18px] ml-[10px] pl-[5px] pr-[5px] text-[13px]">
+              Admin
+            </button>
+            <div className="flex justify-between">
+              <p className="text-[#6B7280]">Regional Paradigm Technician</p>
+
+              <img
+                src={image1}
+                alt="dxgkjl"
+                className="h-[40px] rounded-[50px] absolute bottom-[600px] left-[610px] top-[255px]"
+              />
+            </div>
+          </div>
+          <div className="flex">
+            <div className="border-t-[1px] border-solid border-[#E5E7EB] border-r-[1px] h-[51px]  w-[200px]">
+              <div className="flex ml-[60px] mt-[15px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#9CA3AF"
+                  aria-hidden="true"
+                  class="nz sb axp"
+                  className="w-[20px] "
+                >
+                  <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z"></path>
+                  <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z"></path>
+                </svg>
+                <p className="ml-[10px]">Email</p>
+              </div>
+            </div>
+
+            <div className="border-t-[1px] border-solid border-[#E5E7EB] h-[51px]  w-[200px] ">
+              <div className="flex ml-[60px] mt-[15px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#9CA3AF"
+                  aria-hidden="true"
+                  class="nz sb axp"
+                  className="w-[20px] "
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <p className="ml-[10px]">Call</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-[#FFFFFF] h-[140px] w-[400px] rounded-lg mt-[30px]  ml-[60px] ">
+          <div className=" p-[20px]">
+            Cody Fisher
+            <button className="bg-[#F0FDF4] text-[#15803D] border-[#C4ECD2] border-[1px] rounded-[18px] ml-[10px] pl-[5px] pr-[5px] text-[13px]">
+              Admin
+            </button>
+            <div className="flex justify-between">
+              <p className="text-[#6B7280]">Product Directives Officer</p>
+
+              <img
+                src={image2}
+                alt="dxgkjl"
+                className="h-[40px] rounded-[50px] absolute bottom-[600px] left-[1045px] top-[255px]"
+              />
+            </div>
+          </div>
+          <div className="flex">
+            <div className="border-t-[1px] border-solid border-[#E5E7EB] border-r-[1px] h-[51px]  w-[200px]">
+              <div className="flex ml-[60px] mt-[15px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#9CA3AF"
+                  aria-hidden="true"
+                  class="nz sb axp"
+                  className="w-[20px] "
+                >
+                  <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z"></path>
+                  <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z"></path>
+                </svg>
+                <p className="ml-[10px]">Email</p>
+              </div>
+            </div>
+
+            <div className="border-t-[1px] border-solid border-[#E5E7EB] h-[51px]  w-[200px] ">
+              <div className="flex ml-[60px] mt-[15px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#9CA3AF"
+                  aria-hidden="true"
+                  class="nz sb axp"
+                  className="w-[20px] "
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <p className="ml-[10px]">Call</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-[#FFFFFF] h-[140px] w-[400px] rounded-lg mt-[30px]  ml-[40px] ">
+          <div className=" p-[20px]">
+            Esther Howard
+            <button className="bg-[#F0FDF4] text-[#15803D] border-[#C4ECD2] border-[1px] rounded-[18px] ml-[10px] pl-[5px] pr-[5px] text-[13px]">
+              Admin
+            </button>
+            <div className="flex justify-between">
+              <p className="text-[#6B7280]">Forward Response Developer</p>
+
+              <img
+                src={image3}
+                alt="dxgkjl"
+                className="h-[40px] rounded-[50px] absolute bottom-[600px] left-[1500px] top-[255px]"
+              />
+            </div>
+          </div>
+          <div className="flex">
+            <div className="border-t-[1px] border-solid border-[#E5E7EB] border-r-[1px] h-[51px]  w-[200px]">
+              <div className="flex ml-[60px] mt-[15px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#9CA3AF"
+                  aria-hidden="true"
+                  class="nz sb axp"
+                  className="w-[20px] "
+                >
+                  <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z"></path>
+                  <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z"></path>
+                </svg>
+                <p className="ml-[10px]">Email</p>
+              </div>
+            </div>
+
+            <div className="border-t-[1px] border-solid border-[#E5E7EB] h-[51px]  w-[200px] ">
+              <div className="flex ml-[60px] mt-[15px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#9CA3AF"
+                  aria-hidden="true"
+                  class="nz sb axp"
+                  className="w-[20px] "
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <p className="ml-[10px]">Call</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-[#FFFFFF] h-[140px] w-[400px] rounded-lg mt-[30px]  ml-[90px] ">
+          <div className=" p-[20px]">
+            Jenny Wilson
+            <button className="bg-[#F0FDF4] text-[#15803D] border-[#C4ECD2] border-[1px] rounded-[18px] ml-[10px] pl-[5px] pr-[5px] text-[13px]">
+              Admin
+            </button>
+            <div className="flex justify-between">
+              <p className="text-[#6B7280]">Central Security Manager</p>
+
+              <img
+                src={image4}
+                alt="dxgkjl"
+                className="h-[40px] rounded-[50px] absolute bottom-[300px] left-[615px] top-[455px]"
+              />
+            </div>
+          </div>
+          <div className="flex">
+            <div className="border-t-[1px] border-solid border-[#E5E7EB] border-r-[1px] h-[51px]  w-[200px]">
+              <div className="flex ml-[60px] mt-[15px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#9CA3AF"
+                  aria-hidden="true"
+                  class="nz sb axp"
+                  className="w-[20px] "
+                >
+                  <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z"></path>
+                  <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z"></path>
+                </svg>
+                <p className="ml-[10px]">Email</p>
+              </div>
+            </div>
+
+            <div className="border-t-[1px] border-solid border-[#E5E7EB] h-[51px]  w-[200px] ">
+              <div className="flex ml-[60px] mt-[15px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#9CA3AF"
+                  aria-hidden="true"
+                  class="nz sb axp"
+                  className="w-[20px] "
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <p className="ml-[10px]">Call</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-[#FFFFFF] h-[140px] w-[400px] rounded-lg mt-[30px] ml-[80px]">
+          sdgdsg
+        </div>
+        <div className="bg-[#FFFFFF] h-[140px] w-[400px] rounded-lg mt-[30px] ml-[50px]">
+          dsgd
+        </div>
+      </div>
+    </>
+  );
+};
